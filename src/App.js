7d5b36dd637da14384b7b822457f96bb7c6fa1cc
Boggs <evan.boggs@mantech.com>
2020-03-10 10:18:34 -0400

    icons and stlye

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import Fullscreen from "react-full-screen";
+import { AiOutlineFullscreen } from "react-icons/ai";
+import { GoScreenFull } from "react-icons/go";
 import "./App.css";
 
 function App() {
   const [isFull, goFull] = useState(false);
+
+  const style = {
+    width: "100px",
+    height: "100px"
+  };
 
   return (
     <div className="App">
@@ -13,6 +20,18 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           {!isFull ? (
             <button onClick={() => goFull(true)}>Go Fullscreen</button>
+          ) : null}
+          <br />
+          {!isFull ? (
+            <button onClick={() => goFull(true)}>
+              <AiOutlineFullscreen style={style} />
+            </button>
+          ) : null}
+          <br />
+          {!isFull ? (
+            <button onClick={() => goFull(true)}>
+              <GoScreenFull style={style} />
+            </button>
           ) : null}
         </header>
       </Fullscreen>
